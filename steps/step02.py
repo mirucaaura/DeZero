bff5820f5d105e2307dd2f4c1449ef7ecc8fe185
mirucaaura <yasunari.hikima@gmail.com>
2022-02-08 21:38:16 +0900

    step02

---
+++ steps/step02.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+class Variable:
+    def __init__(self, data) -> None:
+        self.data = data
+
+
+class Function:
+    def __call__(self, input: Variable) -> Variable:
+        x = input.data
+        y = x ** 2
+        output = Variable(y)
+        return output
+
+    def forward(self, in_data):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+
+x = Variable(np.array(10.1))
+f = Square()
+y = f(x)
+print(type(y))
+print(y.data)
